8aaf1400acf93ebf8e2a79ce4fe64fdde4ff723a
1kkiren <1kkiren@mail.ru>
2023-06-13 03:58:35 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,55 +1,58 @@
 let hasScrolled = false;
-        const homeButton = document.getElementById('home');
-        const homeSection = document.getElementById('home-section');
-        const aboutButton = document.getElementById('about');
-        const aboutSection = document.getElementById('about-section');
-        const skillsButton = document.getElementById('skills');
-        const skillsSection = document.getElementById('skills-section');
-        const projectsButton = document.getElementById('projects');
-        const projectsSection = document.getElementById('projects-section');
-        const contactButton = document.getElementById('contact');
-        const contactSection = document.getElementById('contact-section');
-        const socialButton = document.getElementById('social');
-        const socialSection = document.getElementById('social-media-section');
+const homeButton = document.getElementById('home');
+const homeSection = document.getElementById('home-section');
+const aboutButton = document.getElementById('about');
+const aboutSection = document.getElementById('about-section');
+const skillsButton = document.getElementById('skills');
+const skillsSection = document.getElementById('skills-section');
+const projectsButton = document.getElementById('projects');
+const projectsSection = document.getElementById('projects-section');
+const contactButton = document.getElementById('contact');
+const contactSection = document.getElementById('contact-section');
+const socialButton = document.getElementById('social');
+const socialSection = document.getElementById('social-media-section');
 
 
 
-        window.onscroll = function() {
-        const header = document.getElementById("header");
-            if (window.pageYOffset > 0) {
-                header.classList.add("shrink");
-                if (hasScrolled) {
-                    return;
-                } else {
-                    hasScrolled = true;
-                    homeSection.scrollIntoView({ behavior: 'smooth' });
-                }
-            } else {
-                header.classList.remove("shrink");
-                hasScrolled = false;
-            }
-        }
-
-        aboutButton.addEventListener('click', () => {
-            aboutSection.scrollIntoView({ behavior: 'smooth' });
-        });
-
-        homeButton.addEventListener('click', () => {
+window.onscroll = function() {
+const header = document.getElementById("header");
+    if (window.scrollY > 0) {
+        header.classList.add("shrink");
+        if (hasScrolled) {
+            return;
+        } else {
+            hasScrolled = true;
             homeSection.scrollIntoView({ behavior: 'smooth' });
-        });
+        }
+    } else {
+        header.classList.remove("shrink");
+        hasScrolled = false;
+    }
+}
 
-        skillsButton.addEventListener('click', () => {
-            skillsSection.scrollIntoView({ behavior: 'smooth' });
-        });
+aboutButton.addEventListener('click', () => {
+    aboutSection.scrollIntoView({ behavior: 'smooth' });
+});
 
-        projectsButton.addEventListener('click', () => {
-            projectsSection.scrollIntoView({ behavior: 'smooth' });
-        });
+homeButton.addEventListener('click', () => {
+    homeSection.scrollIntoView({ behavior: 'smooth' });
+});
 
-        contactButton.addEventListener('click', () => {
-            contactSection.scrollIntoView({ behavior: 'smooth' });
-        });
+skillsButton.addEventListener('click', () => {
+    skillsSection.scrollIntoView({ behavior: 'smooth' });
+});
 
-        socialButton.addEventListener('click', () => {
-            socialSection.scrollIntoView({ behavior: 'smooth' });
-        });
+projectsButton.addEventListener('click', () => {
+    projectsSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+contactButton.addEventListener('click', () => {
+    contactSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+socialButton.addEventListener('click', () => {
+    socialSection.scrollIntoView({ behavior: 'smooth' });
+});
+
+
+
