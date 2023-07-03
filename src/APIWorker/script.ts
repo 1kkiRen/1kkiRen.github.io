8fcedd8e9cc6e9445d84c01cd6fe63f5308d2148
1kkiren <1kkiren@mail.ru>
2023-07-03 12:52:50 +0300

    [Update] fromNow function added to APIWorker

--- src/APIWorker/script.ts
+++ src/APIWorker/script.ts
@@ -63,8 +63,7 @@ submit.addEventListener('click', () => {
                             image.src = data.img;
                             image.alt = data.alt;
                             date.textContent = new Date(data.year, data.month, data.day).toLocaleDateString();
-                            // dateFromPublish.textContent = formatDistanceToNow(new Date(data.year, data.month, data.day), { addSuffix: true });
-                            
+                            dateFromPublish.textContent = formatDistanceToNow(new Date(data.year, data.month, data.day), { addSuffix: true });
                         })
                     })
                     .catch((error: Error) => {
